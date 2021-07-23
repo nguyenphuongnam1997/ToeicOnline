56aa5b60b9e3dcad982b6ab06a0a4aafb0a6e393
namnguyen <namnpp1997@gmail.com>
2021-07-23 16:06:02 +0700

    dgfd

---
+++ toeic-web-logic/src/main/java/com/namnguyen/web/logic/common/WebConstant.java
@@ -0,0 +1,5 @@
+package com.namnguyen.web.logic.common;
+//chứa các hằng số dùng chung trong controller
+public class WebConstant {
+
+}
---
+++ toeic-web/target/toeic-web-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>NAM NGUYEN</title>
+</head>
+<body>
+<h1> Toeic NAM NGUYEN</h1>
+</body>
+</html>
