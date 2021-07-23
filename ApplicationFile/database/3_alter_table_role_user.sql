0418ed6c1b4e6b03cdee4daa7c6c3e4e2a75551d
namnguyen <namnpp1997@gmail.com>
2021-07-23 16:18:25 +0700

    delete

--- ApplicationFile/database/3_alter_table_role_user.sql
+++
@@ -1,3 +0,0 @@
-alter table toeiconline.user add column roleid bigint;
-
-alter table toeiconline.user add constraint fk_user_role foreign key (roleid) references role(roleid);
--- toeic-web/target/toeic-web-1.0-SNAPSHOT/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>NAM NGUYEN</title>
-</head>
-<body>
-<h1> Toeic NAM NGUYEN</h1>
-</body>
-</html>
